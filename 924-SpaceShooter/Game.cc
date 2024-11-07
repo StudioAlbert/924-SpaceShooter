051b3e3d7d1b57ff65b73d0573e0aab8653a4389
StudioAlbert <account@studio-albert.com>
2024-11-07 22:10:15 +0100

    Game.cc ???

--- 924-SpaceShooter/Game.cc
+++ 924-SpaceShooter/Game.cc
@@ -10,6 +10,7 @@ constexpr float kCooldown_limit_ = 0.15f;
 Game::Game()
 {
 	window_.create(sf::VideoMode(1280, 720), "Space Shooter");
+	//window_.create(sf::VideoMode::getDesktopMode(), "Space Shooter", sf::Style::Fullscreen);
 }
 
 void Game::Loop()
@@ -17,19 +18,24 @@ void Game::Loop()
 
 	window_.setMouseCursorVisible(false);
 
-	float dt = 0.016f;
+	double dt = 0.016f;
 
 	while (window_.isOpen())
 	{
 
 		if (sf::Joystick::isButtonPressed(0, 0) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space) || sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
-			player_missiles_.Spawn(starship_.GetPosition(), { 1500, 0 });
+			if(starship_.IsShootReady())
+			{
+				player_missiles_.Spawn(starship_.GetPosition(), { 1500, 0 });
+				starship_.ShootConfirm();
+			}
 		}
 
 		// what's the current position of the X and Y axes of joystick number 0?
 		const float x = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
 		const float y = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+
 		if (x * x + y * y > 0)
 			starship_.Move({ x / 100.0f , y / 100.0f }, dt);
 
@@ -53,26 +59,31 @@ void Game::Loop()
 				window_.close();
 
 			if (event.type == sf::Event::MouseMoved)
-			{
 				starship_.SetPosition(sf::Vector2u(event.mouseMove.x, event.mouseMove.y));
-			}
 
 			// Sandbox test
 			if (event.type == sf::Event::JoystickButtonReleased)
-			{
 				std::cout << "Button ID #" << event.joystickButton.button << '\n';
-			}
+
 			if (event.type == sf::Event::JoystickMoved)
-			{
 				std::cout << "Axis ID #" << event.joystickMove.axis << '\n';
+
+			if(event.type == sf::Event::KeyPressed)
+			{
+				if(event.key.code == sf::Keyboard::Escape)
+				{
+					// Fullscreen <=> not fullscreen
+					window_.close();
+				}
 			}
 
 		}
 
-		
-
-		player_missiles_.Refresh(dt, window_.getSize());
+		projectiles_.Refresh(dt, window_.getSize());
 		asteroids_.Refresh(dt, window_.getSize());
+		enemy_missiles_.Refresh(dt, window_.getSize());
+		enemy_manager_.Refresh(dt, window_.getSize(), enemy_missiles_);
+
 
 		enemy_missiles_.Refresh(dt, window_.getSize());
 		enemy_manager_.Refresh(dt, window_.getSize(), enemy_missiles_);
@@ -94,6 +105,7 @@ void Game::Loop()
 
 		// At the end of the game loop
 		dt = clock_.restart().asSeconds();
+		//std::cout << "dt ? : " << dt << std::endl;
 
 	}
 
