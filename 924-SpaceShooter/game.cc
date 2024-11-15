e333a937a7d45c3f77fb8861baa8682f59354dbd
StudioAlbert <account@studio-albert.com>
2024-11-15 13:32:06 +0100

    fini fini

--- 924-SpaceShooter/game.cc
+++ 924-SpaceShooter/game.cc
@@ -5,12 +5,25 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 
-constexpr float kCooldown_limit_ = 0.15f;
-
 Game::Game()
 {
 	window_.create(sf::VideoMode(1280, 720), "Space Shooter");
 	//window_.create(sf::VideoMode::getDesktopMode(), "Space Shooter", sf::Style::Fullscreen);
+
+	font_.loadFromFile("Assets\\Fonts\\spaceboy\\SpaceBoy.ttf");
+	score_.setFont(font_); 	// select the font
+	score_.setCharacterSize(24); // set the character size in pixels, not points!
+	score_.setFillColor(sf::Color::White);// set the color
+	score_.setPosition(50, 50);
+
+	lives_.setFont(font_); 	// select the font
+	lives_.setCharacterSize(24); // set the character size in pixels, not points!
+	lives_.setFillColor(sf::Color::White);// set the color
+	lives_.setPosition(window_.getSize().x - 200, 50);
+
+	score_.setString("Score : ");// set the string to display
+	lives_.setString("Lives : ");// set the string to display
+
 }
 
 void Game::Loop()
@@ -19,13 +32,14 @@ void Game::Loop()
 	window_.setMouseCursorVisible(false);
 
 	double dt = 0.016f;
+	int score_points = 0;
 
 	while (window_.isOpen())
 	{
 
 		if (sf::Joystick::isButtonPressed(0, 0) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space) || sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
-			if(starship_.IsShootReady())
+			if(starship_.GetHp() > 0 && starship_.IsShootReady())
 			{
 				player_missiles_.Spawn(starship_.GetPosition(), { 1500, 0 });
 				starship_.ShootConfirm();
@@ -79,27 +93,47 @@ void Game::Loop()
 
 		}
 
+		if(starship_.GetHp() > 0)
+		{
+			starship_.Refresh(dt);
+		}
 		asteroids_.Refresh(dt, window_.getSize());
+		player_missiles_.Refresh(dt, window_.getSize());
 		enemy_missiles_.Refresh(dt, window_.getSize());
 		enemy_manager_.Refresh(dt, window_.getSize(), enemy_missiles_);
 
+		if(starship_.GetHp() > 0)
+		{
+			starship_.CheckCollisions(asteroids_.GetEntities());
+			starship_.CheckCollisions(enemy_manager_.GetEntities());
+			starship_.CheckCollisions(enemy_missiles_.GetEntities());
+		}
 
-		enemy_missiles_.Refresh(dt, window_.getSize());
-		enemy_manager_.Refresh(dt, window_.getSize(), enemy_missiles_);
-
-		starship_.CheckCollisions(asteroids_.GetEntities());
-		starship_.CheckCollisions(enemy_manager_.GetEntities());
-		starship_.CheckCollisions(enemy_missiles_.GetEntities());
-
-		player_missiles_.CheckCollisions(asteroids_.GetEntities());
-		player_missiles_.CheckCollisions(enemy_manager_.GetEntities());
+		score_points += player_missiles_.CheckCollisions(asteroids_.GetEntities());
+		score_points += 5 * player_missiles_.CheckCollisions(enemy_manager_.GetEntities());
+		
+		score_.setString("Score : " + std::to_string(score_points));
+		lives_.setString("Lives : " + std::to_string(starship_.GetHp()));
 
 		window_.clear();
 		window_.draw(player_missiles_);
 		window_.draw(enemy_missiles_);
 		window_.draw(asteroids_);
 		window_.draw(enemy_manager_);
-		window_.draw(starship_);
+
+		if(starship_.GetHp() > 0)
+		{
+			window_.draw(starship_);
+		}else
+		{
+			// GAME OVER
+			// Music de game over
+			// Texte de game over
+			// etc......
+		}
+
+		window_.draw(score_);
+		window_.draw(lives_);
 		window_.display();
 
 		// At the end of the game loop
